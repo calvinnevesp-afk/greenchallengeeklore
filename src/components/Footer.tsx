e326ab8ef468776d589be0d314e2cc851411cc15
calvinnevesp-afk <calvin.nevesp@icloud.com>
2026-03-17 16:17:04 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer id="contact">
+      <p>&copy; 2026 Green Challenge. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
